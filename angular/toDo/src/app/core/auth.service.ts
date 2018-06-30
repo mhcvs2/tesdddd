419b8beb1fb4990cc37e78a8b576c9a06fe9ad79
mhcvs2 <merzhong0501@163.com>
2018-06-30 23:10:40 +0800

    add

---
+++ angular/toDo/src/app/core/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+  loginWithCredentials(username: string, password: string): boolean {
+    return username == "wangpeng";
+  }
+
+}
